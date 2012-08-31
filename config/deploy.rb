fda4fffc0c003dd63c0ff7af6dba62ba7aad261a
xecutioner <xecutioner.kapil@gmail.com>
2012-09-01 00:35:08 +0545

    added capify

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :application, "webcast"
+set :repository,  "git@github.com:revol-tech/software-freedom-day.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "revol-tech.com.np"                          # Your HTTP server, Apache/etc
+role :app, "revol-tech.com.np"                          # This may be the same as your `Web` server
+# role :db,  "revol-tech.com.np", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
