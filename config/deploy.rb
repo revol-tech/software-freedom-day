28f49ec69871584341503c4caa6827770fa525e3
xecutioner <xecutioner.kapil@gmail.com>
2012-09-01 00:38:35 +0545

    capified properly and path added

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,10 @@ set :repository,  "git@github.com:revol-tech/software-freedom-day.git"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
+set :branch, 'master'
 role :web, "revol-tech.com.np"                          # Your HTTP server, Apache/etc
-role :app, "revol-tech.com.np"                          # This may be the same as your `Web` server
+role :app, "revol-tech.com.np"   
+set :deploy_to, "/home/deploy/www/webcast.revol-tech.com.np"                       # This may be the same as your `Web` server
 # role :db,  "revol-tech.com.np", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
@@ -23,3 +24,5 @@ role :app, "revol-tech.com.np"                          # This may be the same a
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+after "deploy","deploy:restart"
